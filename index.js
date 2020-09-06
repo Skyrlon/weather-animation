35873dc0e8c6b50f17c7178e760e162ac86accf7
Antoine Drouault <antoinedrouault@hotmail.com>
2020-09-06 23:12:35 +0200

    translated all html, variables scss and javascript elements in same language (english)

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
 let eltBtn = document.getElementsByClassName("btn");
 
-let eltChemin = document.getElementsByClassName("chemin");
-let eltCheminStatus = document.getElementsByClassName("chemin-status");
-let eltHerbe = document.getElementsByClassName("herbe");
-let eltCiel = document.getElementsByClassName("ciel");
-let eltSoleil = document.getElementsByClassName("soleil");
-let eltContainer = document.getElementsByClassName("container");
-let eltCorps = document.getElementsByClassName("corps");
-let eltPaupiere = document.getElementsByClassName("paupiere");
-let eltLevre = document.getElementsByClassName("levre");
-let eltBrasGauche = document.getElementsByClassName("bras_gauche");
-let eltBrasGaucheInferieur = document.getElementsByClassName("bras_gauche-inferieur");
-let eltCoucou = document.getElementsByClassName("coucou");
+let road = document.getElementsByClassName("road");
+let roadStatus = document.getElementsByClassName("road-status");
+let grass = document.getElementsByClassName("grass");
+let sky = document.getElementsByClassName("sky");
+let sun = document.getElementsByClassName("sun");
+let container = document.getElementsByClassName("container");
+let body = document.getElementsByClassName("body");
+let eyelid = document.getElementsByClassName("eyelid");
+let lip = document.getElementsByClassName("lip");
+let armLeft = document.getElementsByClassName("arm_left");
+let armLeftInferior = document.getElementsByClassName("arm_left-inferior");
+let hello = document.getElementsByClassName("hello");
 
 eltBtn[0].addEventListener("click", function () {
     
-    eltChemin[0].style.animation = "chemin-deroule 1s both";
-    eltCheminStatus[0].style.animation = "chemin-status 1s both";
-    eltHerbe[0].style.animation = "herbe-etendre 1s 1s forwards";
-    eltCiel[0].style.animation = "ciel-monter 1s 2s forwards";
-    eltSoleil[0].style.animation = "soleil-monter 1s 3s forwards";
-    eltContainer[0].style.animation = "container-monter 1s 4s forwards";
-    eltCorps[0].style.animation = "corps-rotate 1s 5s forwards";
-    for (let i = 0; i < eltPaupiere.length; i++) {
-        eltPaupiere[i].style.animation = "visage-decouvert 1s 6s forwards";
+    road[0].style.animation = "road-rollout 1s both";
+    roadStatus[0].style.animation = "road-status 1s both";
+    grass[0].style.animation = "grass-expansion 1s 1s forwards";
+    sky[0].style.animation = "sky-ascend 1s 2s forwards";
+    sun[0].style.animation = "sun-ascend 1s 3s forwards";
+    container[0].style.animation = "container-showup 1s 4s forwards";
+    body[0].style.animation = "body-rotate 1s 5s forwards";
+    for (let i = 0; i < eyelid.length; i++) {
+        eyelid[i].style.animation = "face-uncovered 1s 6s forwards";
     };
-    for (let i = 0; i < eltLevre.length; i++) {
-        eltLevre[i].style.animation = "visage-decouvert 1s 6s forwards";
+    for (let i = 0; i < lip.length; i++) {
+        lip[i].style.animation = "face-uncovered 1s 6s forwards";
     };
-    eltBrasGauche[0].style.animation = "bras-leve 0.7s 7s forwards"
-    eltBrasGaucheInferieur[0].style.animation = "wave 1s 7.3s ease-in-out alternate infinite both";
-    eltCoucou[0].style.animation = "coucou-apparition 1s 8s both";
+    armLeft[0].style.animation = "arm-raised 0.7s 7s forwards"
+    armLeftInferior[0].style.animation = "wave 1s 7.3s ease-in-out alternate infinite both";
+    hello[0].style.animation = "hello-appearance 1s 8s both";
 });
