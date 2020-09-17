ec803483a4abc3d5ef005008c6d080e51818bed7
Antoine Drouault <antoinedrouault@hotmail.com>
2020-09-17 23:44:26 +0200

    added tornado element

--- index.js
+++ index.js
@@ -57,3 +57,33 @@ function drawTieBottom() {
 
 drawSuitShirt();
 drawTieBottom();
+
+const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+let tornado1 = document.getElementsByClassName('tornado')[0];
+let tornado2 = document.getElementsByClassName('tornado')[1];
+let tornado3 = document.getElementsByClassName('tornado')[2];
+let tornado4 = document.getElementsByClassName('tornado')[3];
+
+for (let i = 0; i < 24; i++) {
+    let div = document.createElement('div');
+    div.setAttribute('class', alphabet[i]);
+    tornado1.appendChild(div);
+}
+
+for (let i = 0; i < 24; i++) {
+    let div = document.createElement('div');
+    div.setAttribute('class', alphabet[i]);
+    tornado2.appendChild(div);
+}
+
+for (let i = 0; i < 24; i++) {
+    let div = document.createElement('div');
+    div.setAttribute('class', alphabet[i]);
+    tornado3.appendChild(div);
+}
+
+for (let i = 0; i < 24; i++) {
+    let div = document.createElement('div');
+    div.setAttribute('class', alphabet[i]);
+    tornado4.appendChild(div);
+} 
